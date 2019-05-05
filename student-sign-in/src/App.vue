e2c706dbc49943121c91f5a86652bfb4d1eaea91
rahwagebrehiwot <ot6584tk@go.minneapolis.edu>
2019-05-05 13:10:36 -0500

    add Would_You_Rather.html.

---
+++ student-sign-in/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <NewStudentForm v-on:student-added="newStudentAdded"></NewStudentForm>
+    <StudentTable v-bind:students="students" v-on:student-present="studentArrivedOrLeft" v-on:delete-student="studentDeleted"></StudentTable>
+    <StudentMessage v-bind:message="message" v-bind:name="name"> </StudentMessage>  </div>
+</template>
+
+<script>
+import NewStudentForm from "./components/NewStudentForm";
+import StudentTable from "./components/StudentTable";
+import StudentMessage from "./components/StudentMessage";
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      students: [],
+      message: '',
+      name: ''
+    }
+  },
+  components: {
+    NewStudentForm,
+    StudentTable,
+    StudentMessage,
+  },
+  methods: {
+    newStudentAdded(student) {
+      this.students.push(student)
+      this.students.sort(function(s1, s2) {
+        return s1.name.toLowerCase() < s2.name.toUpperCase() ? -1 : 1
+      })
+    },
+    studentArrivedOrLeft(student) {
+      this.message = student.present ? 'Welcome,' : 'Goodbye,'
+      this.name = student.name
+    },
+    studentDeleted(student){
+      this.students=this.students.filter(function (s) {return s!=student})
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
